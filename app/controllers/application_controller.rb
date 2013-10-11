fe72e849abbd73f747cfcba365f2f32ecb2bac1a
Giovanni Messina <gmgp.info@gmail.com>
2013-10-11 12:03:27 +0200

    Define helper methods for the concept of current_user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    u = nil
+    if !!session[:user_id]
+      begin
+        u = User.find(session[:user_id])
+      rescue ActiveRecord::RecordNotFound => e
+        session[:user_id] = nil
+      end
+    end
+    u
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
