b4a67fe4043263718eb74b77e8296fcda9b32832
Giovanni Messina <gmgp.info@gmail.com>
2013-10-11 11:30:25 +0200

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2SignetRails::Application.config.secret_token = '8e0664b864a264811cd86cf908361e8efc42d1b7d94017b1b666f342f293382c6ffde9a75df0abdcdb683c3c1024eff2b81444b2fcc39514edff6978c4434eb1'
